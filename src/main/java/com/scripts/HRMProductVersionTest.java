5cd343e1c9906f2e32533086c61fc713acdde15b
Ramarao <bhanud7111@gmail.com>
2021-03-27 14:45:28 +0530

    version

---
+++ src/main/java/com/scripts/HRMProductVersionTest.java
@@ -0,0 +1,24 @@
+package com.scripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class HRMProductVersionTest {
+	@Test
+	public void productVersion()
+	{
+		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/dashboard");
+		driver.manage().window().maximize();
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		WebElement version = driver.findElement(By.xpath("//div[text()[contains(.,'OrangeHRM 4.7')]]"));
+	    System.out.println(version.getText());
+	
+	}
+
+}
