e273a239c862bd2bdb41a0e71af2b8687ed846f4
Ramarao <bhanud7111@gmail.com>
2021-05-02 15:37:58 +0530

    switched to child window

--- src/main/java/com/scripts/Linkedin.java
+++ src/main/java/com/scripts/Linkedin.java
@@ -1,11 +1,14 @@
 package com.scripts;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Linkedin {
-
+public static String parent,child;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
@@ -15,8 +18,21 @@ public class Linkedin {
 		System.out.println(driver.getCurrentUrl());
 		System.out.println(driver.getTitle());
 		driver.findElement(By.xpath("//img[@alt='LinkedIn OrangeHRM group']")).click();
+		parent=driver.getWindowHandle();
+		Set<String> all = driver.getWindowHandles();
+		Iterator<String> i = all.iterator();
+		while(i.hasNext())
+		{
+			child=i.next();
+			if(!parent.equals(child))
+			{
+				driver.switchTo().window(child);
+			}
+		}
+		
 		System.out.println("Title of the Linked in Page"+driver.getTitle());
 		System.out.println("Child Window URL"+driver.getCurrentUrl());
+		
 	}
 
 }
